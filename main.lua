dcf164b4c879d94c2077a665a8118fb910223391
joelleishimwe <joelle.ishimwe@ocsbstudent.ca>
2018-03-08 10:43:29 -0500

    Added header comment

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
+-- Title: TouchAndReact
+-- Name: Joelle Ishimwe
+-- Course: ICS2O/3C
+-- This program displays a button that when I click on it, it turns colour and displays text another object
 -----------------------------------------------------------------------------------------
 
 -- Your code here
